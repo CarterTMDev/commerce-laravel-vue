77a7db135a6b7eccdee1012c5fc30320e9cab2f9
Carter McCall <86985619+CarterTMDev@users.noreply.github.com>
2022-01-15 23:18:24 -0600

    - removed primary key modifier because id() is primary key by default

--- database/migrations/2022_01_16_010447_create_customers_table.php
+++ database/migrations/2022_01_16_010447_create_customers_table.php
@@ -14,7 +14,7 @@ class CreateCustomersTable extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
